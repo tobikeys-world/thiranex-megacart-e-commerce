32501096a1ed2cf363849c5d95651f163c5f1c96
tobikeys-world <tobikkeys@gmail.com>
2026-09-23 12:07:01 +0100

    fixed render port

--- backend/server.js
+++ backend/server.js
@@ -40,6 +40,6 @@ app.get("/api/admin-test", protect, adminOnly, (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`MegaaCart server running on port ${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`MegaCart server running on port ${PORT}`);
 });
